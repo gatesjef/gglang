ddc024a270db12299ae013d1474b7db9f27e66d3
gatesjef <gatesjef@hotmail.com>
2014-10-13 01:32:34 -0700

    main.gg synced with compiler.
    done:
    - function
    - scoped varaibles
    - function calls

--- gg_compiler/gg_llvm.h
+++
@@ -1,4 +0,0 @@
-// gg_llvm.h
-#pragma once
-
-void llvm_emit_program(const struct ParseOutput &program);
--- gg_compiler/precompile.cpp
+++
@@ -1,8 +0,0 @@
-// precompile.cpp : source file that includes just the standard includes
-// gg_compiler.pch will be the pre-compiled header
-// precompile.obj will contain the pre-compiled type information
-
-#include "precompile.h"
-
-// TODO: reference any additional headers you need in precompile.h
-// and not in this file
